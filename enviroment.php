4f98e8d6d1f3fb72588c2903451c0fa88c15f679
Agustin-Basilio-Matijasevich <agustinbasiliomatijasevich@gmail.com>
2022-11-24 17:12:26 -0300

    Update enviroment.php
    Actualizacion de cabeceras para solucionar problemas de conexion desde javascript

--- enviroment.php
+++ enviroment.php
@@ -1,5 +1,11 @@
 <?php
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('content-type: application/json; charset=utf-8');
+
 $DB = new mysqli("electronicanordeste.tplinkdns.com", "appblog", "placido", "blog_php", 42321);
 $serverStatus = $DB->connect_errno;
-header('Access-Control-Allow-Origin:*');
+
 ?>
