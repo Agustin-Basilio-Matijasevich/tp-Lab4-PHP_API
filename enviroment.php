2bbb7aaafd7add55948823e46a367ba28d791c0c
Agustin-Basilio-Matijasevich <agustinbasiliomatijasevich@gmail.com>
2022-11-24 16:59:37 -0300

    Update enviroment.php
    Se añade header debido a error consumiendo la API desde VUE

--- enviroment.php
+++ enviroment.php
@@ -1,4 +1,5 @@
 <?php
 $DB = new mysqli("electronicanordeste.tplinkdns.com", "appblog", "placido", "blog_php", 42321);
 $serverStatus = $DB->connect_errno;
+header('Access-Control-Allow-Origin:*');
 ?>
